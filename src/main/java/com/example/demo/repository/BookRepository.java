698ac7b4987ff1d1873246fac8aab682fe8e37ae
Artem Surzhenko <114075751+artemsa223@users.noreply.github.com>
2024-02-25 18:34:08 +0000

    Update BookRepository.java

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,11 +1,1 @@
-package com.example.demo.repository;
 
-import com.example.demo.model.Book;
-import java.util.List;
-
-public interface BookRepository {
-    Book save(Book book);
-
-    List<Book> findAll();
-
-}
