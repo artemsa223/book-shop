3e261738f600152446c10114be32931adad2a59f
Artem Surzhenko <114075751+artemsa223@users.noreply.github.com>
2024-02-25 18:41:23 +0000

    Update BookService.java

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,12 +1,1 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Book;
-import java.util.List;
-
-public interface BookService {
-    Book save(Book book);
-
-    List<Book> findAll();
-
-}
 
