b28ef16f72fa9094f738250214e9e5f23d4aaaab
artem <artemsa223@gmail.com>
2024-02-25 18:44:35 +0000

    implementation Book, BookDao,BookService

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,1 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Book;
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+
+    List<Book> findAll();
+
+}
 
