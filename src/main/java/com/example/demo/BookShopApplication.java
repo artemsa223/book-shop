57d390f3cded0d01665c44c1bf955e45191aa708
artem <artemsa223@gmail.com>
2024-02-25 18:39:10 +0000

    implementation Book, BookDao,BookService

--- src/main/java/com/example/demo/BookShopApplication.java
+++ src/main/java/com/example/demo/BookShopApplication.java
@@ -1,1 +1,35 @@
+package com.example.demo;
 
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BookShopApplication {
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookShopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("The Catcher in the Rye");
+            book.setAuthor("J.D. Salinger");
+            book.setPrice(BigDecimal.TEN);
+            book.setIsbn("1234-5678");
+            book.setDescription("A novel about...");
+            bookService.save(book);
+            bookService.findAll().forEach(System.out::println);
+        };
+    }
+}
