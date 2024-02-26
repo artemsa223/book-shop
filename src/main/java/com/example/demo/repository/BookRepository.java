eb244788b3fe4710e48d692faba7a36de8ae7cc4
artem <artemsa223@gmail.com>
2024-02-26 17:05:26 +0000

    implementation exception, mapper, config, dto, controller

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,11 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
     Book save(Book book);
 
     List<Book> findAll();
+
+    Optional<Book> findById(Long id);
 
 }
 
--- src/main/java/com/example/demo/service/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -1,27 +1,38 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Book;
+import com.example.demo.dto.BookDto;
+import com.example.demo.dto.CreateBookRequestDto;
+import com.example.demo.exception.EntityNotFoundException;
+import com.example.demo.mapper.BookMapper;
 import com.example.demo.repository.BookRepository;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
+
     private final BookRepository bookRepository;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    private final BookMapper bookMapper;
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll()
+                .stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto getById(Long id) {
+        return bookMapper.toDto(bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found by id " + id)));
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto createBook(CreateBookRequestDto requestDto) {
+        return bookMapper.toDto(bookRepository.save(bookMapper.toModel(requestDto)));
     }
 }
