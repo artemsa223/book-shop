b0252640639b683bdc10782495953a7e95408666
artem <artemsa223@gmail.com>
2024-02-27 14:10:54 +0000

    fixes

--- src/main/java/com/example/demo/service/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.BookDto;
 import com.example.demo.dto.CreateBookRequestDto;
 import com.example.demo.exception.EntityNotFoundException;
 import com.example.demo.mapper.BookMapper;
+import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -51,16 +52,8 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto updateById(CreateBookRequestDto requestDto, Long id) {
-        return bookMapper.toDto(bookRepository.findById(id)
-                .map(book -> {
-                    book.setTitle(requestDto.getTitle());
-                    book.setAuthor(requestDto.getAuthor());
-                    book.setIsbn(requestDto.getIsbn());
-                    book.setPrice(requestDto.getPrice());
-                    book.setDescription(requestDto.getDescription());
-                    book.setCoverImage(requestDto.getCoverImage());
-                    return bookRepository.save(book);
-                })
-                .orElseThrow(() -> new EntityNotFoundException("Book not found by id " + id)));
+        Book update = bookMapper.toModel(requestDto);
+        update.setId(id);
+        return bookMapper.toDto(bookRepository.save(update));
     }
 }
