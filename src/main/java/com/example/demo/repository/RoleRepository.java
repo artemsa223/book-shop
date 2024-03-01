caf6e11eb0cce48ee139d63f463afc9387c24e9c
artem <artemsa223@gmail.com>
2024-03-01 17:58:56 +0000

    fixes

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -2,9 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(Role.RoleName role);
 }
